55a0bd6d721cebf5072032a8f04a44c7fb7cba18
NiaAmellya <niaamellya12@gmail.com>
2021-03-17 16:30:18 +0700

    Update

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -5,12 +5,13 @@ class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Item itemArgs = ModalRoute.of(context).settings.arguments;
-    return MaterialApp(
-      home: Scaffold(
-          body: Container(
-        alignment: Alignment.center,
-        child: Text(itemArgs.name + " ; " + itemArgs.price.toString()),
-      )),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Shopping List"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Text(itemArgs.name + " ; " + itemArgs.price.toString()),
+        ));
   }
 }
